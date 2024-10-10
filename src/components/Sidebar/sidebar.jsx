96e4920966c111216616ec6a8e408b1cf2447674
codesprintsite <codesprintsite@gmail.com>
2024-10-10 17:48:35 +0500

    add icons in sidebar component

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,38 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom"; 
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/Logo.png";
-import './Sidebar.css'; 
+import { FaThList, FaListAlt, FaBriefcase, FaUserAlt } from "react-icons/fa"; 
+import { LuLayoutDashboard } from "react-icons/lu"; 
+import './Sidebar.css';
 
 const Sidebar = () => {
+  const location = useLocation();
   const menuItems = [
-    { name: "Dashboard", path: "/" },
-    { name: "Category", path: "/category" },
-    { name: "SubCategory", path: "/subcategory" },
-    { name: "Gigs", path: "/gigs" },
-    { name: "User", path: "/user" },
+    { name: "Dashboard", path: "/", icon: <LuLayoutDashboard /> },
+    { name: "Category", path: "/category", icon: <FaThList /> },
+    { name: "SubCategory", path: "/subcategory", icon: <FaListAlt /> }, 
+    { name: "Gigs", path: "/gigs", icon: <FaBriefcase /> }, 
+    { name: "User", path: "/user", icon: <FaUserAlt /> },
   ];
 
-  return (
-    <div className="font-poppins">
-      <div className="w-64 bg-[#0054ba] text-white p-5 fixed top-0 left-0 h-full z-10">
-        <div className="logo-container w-32 h-32 flex justify-center items-center rounded-full ">
-          <img src={Logo} alt="Logo" className="w-28 rounded-full animate-heart " />
-        </div>
-        {menuItems.map((item) => (
-          <div className="mt-10" key={item.name}>
-            <Link to={item.path}>
-              <h2 className="text-xl font-medium mb-2 cursor-pointer">
-                {item.name}
-              </h2>
-            </Link>
-          </div>
-        ))}
+  const handleLogout = () => {
+    if (window.confirm("Are you sure you want to logout?")) {
+      console.log("User logged out");
+    }
+  };
 
-        <div className="mt-24">
-          <button className="w-full bg-[#618abd] p-2 rounded hover:bg-[#40556e]">
-            Logout
-          </button>
+  return (
+    <div className="w-52 bg-[#0054ba] text-white fixed top-0 left-0 h-full font-poppins z-10">
+      <div className="logo-container w-32 h-32 flex justify-center items-center rounded-full mt-5 ml-5">
+        <img src={Logo} alt="Logo" className="w-28 rounded-full animate-heart" />
+      </div>
+      {menuItems.map((item) => (
+        <div className="mt-7 " key={item.name}>
+          <Link to={item.path}>
+            <h2
+              className={`flex items-center text-xl font-medium mb-2 cursor-pointer p-3 transition duration-300 
+                ${location.pathname === item.path ? "bg-gray-300 text-[#0054ba]" : "hover:bg-gray-300 hover:text-[#0054ba]"}`}
+            >
+              <span className="mr-2">{item.icon}</span> {item.name}
+            </h2>
+          </Link>
         </div>
+      ))}
+
+      <div className="mt-10">
+        <button onClick={handleLogout} className="w-full bg-[#618abd] p-2 rounded hover:bg-[#40556e]">
+          Logout
+        </button>
       </div>
     </div>
   );
