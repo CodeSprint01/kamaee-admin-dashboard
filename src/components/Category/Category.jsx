a0565c27aed66085113bb4373a11b78683e108ff
codesprintsite <codesprintsite@gmail.com>
2024-10-10 14:37:02 +0500

    add table in Category and SubCategory component

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Card } from "@material-tailwind/react";
+import DataTable from "./DataTable";
+
 
 const Category = () => {
+  const TABLE_HEAD = ["Name", "Job", "Employed", "Actions"];
+  const TABLE_ROWS = [
+    { name: "John Michael", job: "Manager", date: "23/04/18" },
+    { name: "Alexa Liras", job: "Developer", date: "23/04/18" },
+    { name: "Laurent Perrier", job: "Executive", date: "19/09/17" },
+    { name: "Michael Levi", job: "Developer", date: "24/12/08" },
+    { name: "Richard Gran", job: "Manager", date: "04/10/21" },
+  ];
   return (
-    <div className='text-6xl font-bold mt-60 ml-8'>
-      Category page is coming soon!
+    <div>
+    <Card className="h-full w-full mt-10 overflow-scroll">
+      <DataTable rows={TABLE_ROWS}  /> 
+    </Card>
+    <div className="mt-4 flex justify-end">
+      <button
+        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+      >
+        Add
+      </button>
     </div>
-  )
-}
+  </div>
+  );
+};
 
-export default Category
+export default Category;
---
+++ src/components/SubCategory/DataTable.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Typography } from "@material-tailwind/react";
+
+const DataTable = ({ rows, onDelete }) => {
+  const TABLE_HEAD = ["Name", "Job", "Employed", "Actions"];
+
+  return (
+    <table className="w-full min-w-max table-auto text-left">
+      <thead>
+        <tr>
+          {TABLE_HEAD.map((head) => (
+            <th
+              key={head}
+              className="border-b border-gray-300 bg-gray-200 p-4"
+            >
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-bold leading-none opacity-70"
+              >
+                {head}
+              </Typography>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(({ name, job, date }, index) => {
+          const isLast = index === rows.length - 1;
+          const classes = isLast ? "p-4" : "p-4 border-b border-gray-300";
+
+          return (
+            <tr key={name} className={index % 2 !== 0 ? "bg-gray-100" : ""}>
+              <td className={classes}>
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {name}
+                </Typography>
+              </td>
+              <td className={classes}>
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {job}
+                </Typography>
+              </td>
+              <td className={classes}>
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {date}
+                </Typography>
+              </td>
+              <td className={classes}>
+                <div className="flex space-x-10">
+                  <Typography as="a" href="#" variant="small" color="blue-gray" className="font-normal">
+                    Edit
+                  </Typography>
+                  <Typography
+                    as="button"
+                    variant="small"
+                    color="red"
+                    className="font-normal"
+                  >
+                    Delete
+                  </Typography>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default DataTable;
